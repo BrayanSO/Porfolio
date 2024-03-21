ad45f95fb2f651daa3ef6e45c0ea2f641c93afe0
Brayan Solera (BlackHeart) <briansolera@gmail.com>
2024-03-21 13:05:40 -0600

    img animation

--- src/components/RotateImage.js
+++ src/components/RotateImage.js
@@ -11,23 +11,32 @@ const RotateImage = ({ imageUrl }) => {
     setStartX(clientX);
     setStartRotation(rotation);
   };
-
   const handleMove = (clientX) => {
     const deltaX = clientX - startX;
-    const maxDeltaX = 100; // Ajusta esto para cambiar la sensibilidad al movimiento
-    const maxRotation = 180; // Ajusta esto para cambiar la velocidad máxima de rotación
-
+    const maxDeltaX = 320; // Ajusta esto para cambiar la sensibilidad al movimiento
+    const maxRotation = 60; // Ajusta esto para cambiar la velocidad máxima de rotación
+  
     // Calcula la velocidad de rotación basada en la distancia recorrida
     const speed = Math.abs(deltaX) / maxDeltaX * maxRotation;
-
+  
+    // Ajusta la velocidad inicial para que inicie girando lentamente
+    const initialSpeed = 10; // Velocidad inicial baja
+    const adjustedSpeed = initialSpeed + (speed - initialSpeed) * 0.5; // Ajusta la velocidad gradualmente
+  
+    // Aplica la velocidad como una clase CSS al elemento de imagen
+    const rotationClass = adjustedSpeed > 0 ? 'coin-rotation' : '';
+    imageRef.current.className = `homepage-image ${rotationClass} ${adjustedSpeed > 50 ? 'fast' : ''} ${adjustedSpeed > 100 ? 'faster' : ''}`;
+  
     // Aplica la rotación considerando la dirección del movimiento
     const direction = deltaX > 0 ? 1 : -1;
-    const newRotation = startRotation + (direction * speed);
-
+    const newRotation = startRotation + (direction * adjustedSpeed);
+  
     // Limita la rotación al rango de -180 a 180 grados
     const clampedRotation = newRotation % 360;
     setRotation(clampedRotation);
   };
+  
+  
 
   const handleTouchStart = (event) => {
     event.preventDefault();
