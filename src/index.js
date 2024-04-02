b24f9b31ad07b7222d8b18b31ba46ec1a9aaa629
Brayan Solera (BlackHeart) <briansolera@gmail.com>
2024-04-02 16:37:08 -0600

    background 1

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Header from "./components/Header.js"
+import App from './components/Particle.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <App></App>
   <Header></Header>
   </React.StrictMode>
 );
